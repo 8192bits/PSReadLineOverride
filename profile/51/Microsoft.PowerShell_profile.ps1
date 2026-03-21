b9030b6bc4608570d86761bf9bed2b8ce77471a5
8192bits <vwin32@tutanota.de>
2026-03-21 21:52:23 +0100

    Update PowerShell profile for user-specific paths

--- profile/51/Microsoft.PowerShell_profile.ps1
+++ profile/51/Microsoft.PowerShell_profile.ps1
@@ -3,7 +3,7 @@ Write-Host "Windows PowerShell 5.1"
 Write-Host "Copyright (C) Microsoft Corporation. All rights reserved."
 Write-Host ""
 
-Import-Module "C:\Users\admin\Documents\WindowsPowerShell\Modules\PSReadLine\2.4.5\PSReadLine.psd1" -Force
+Import-Module "$HOME\Documents\WindowsPowerShell\Modules\PSReadLine\2.4.5\PSReadLine.psd1" -Force
 
 Set-PSReadLineOption -Colors @{
     Command            = "Yellow"
