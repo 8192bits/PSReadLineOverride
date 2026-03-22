5d89114a80661bd4234a3f1c8f3f2c463cd727b6
8192bits <vwin32@tutanota.de>
2026-03-22 19:20:45 +0100

    Remove custom prompt function from PowerShell profile

--- profile/51/Microsoft.PowerShell_profile.ps1
+++ profile/51/Microsoft.PowerShell_profile.ps1
@@ -4,25 +4,3 @@ Write-Host "Copyright (C) Microsoft Corporation. All rights reserved."
 Write-Host ""
 
 Import-Module "$HOME\Documents\WindowsPowerShell\Modules\PSReadLine\2.4.5\PSReadLine.psd1" -Force
-
-Set-PSReadLineOption -Colors @{
-    Command            = "Yellow"
-    Comment            = "DarkGreen"
-    ContinuationPrompt = "Gray"
-    Default            = "Gray"
-    Emphasis           = "Cyan"
-    Error              = "Red"
-    Keyword            = "Green"
-    Member             = "White"
-    Number             = "White"
-    Operator           = "DarkGray"
-    Parameter          = "DarkGray"
-    String             = "DarkCyan"
-    Type               = "Gray"
-    Variable           = "Green"
-}
-
-
-function prompt {
-    "PS $($executionContext.SessionState.Path.CurrentLocation)> "
-}
